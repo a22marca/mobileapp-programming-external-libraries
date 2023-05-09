8706729d78e4f191f70b43106d37e21ef6b434df
a22marca <128985931+a22marca@users.noreply.github.com>
2023-05-09 16:48:57 +0200

    Implementerat en instans av picasso som hämtar en bild som sedan används i en ImageView

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView picassoImageView = findViewById(R.id.picasso_imageview);
+        Picasso.get()
+                .load("https://images.unsplash.com/photo-1679498819175-9b2bc1df5ccf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8")
+                .into(picassoImageView);
+
     }
 }
